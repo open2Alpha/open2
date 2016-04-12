33d616d73cf9cfce66e1db56834cf9b84271e82a
prexsa <prexgit@gmail.com>
2016-04-11 19:49:18 -0700

    [progress] connect login to server to database

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,5 @@
 var express = require('express')
-var router = express.Router()
+var router = express.Router();
 var db = require('../db.js')
 
 
@@ -24,4 +24,4 @@ router.get('/dashboard/:id', function (req, res) {
 	})
 })
 
-module.exports = router
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,22 +2,42 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var db = require('./db.js');
 var cors = require('cors');
-var router = express.Router();
+//var router = express.Router();
 var app = express();
 
 app.use(cors());
 
-
-<-----Routes----->
-var routes = require('./server/routes');
+//var routes = require('routes');
 
 
 app.use(bodyParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '*******')));
+app.use(express.static(__dirname + '/../client'));
+// app.use('/api/', router);
 
 
-var host = process.env.PORT || 8080
+
+app.post('/', function(request, response){
+	var username = request.body.username;
+	var password = request.body.password;
+
+	// db.query('SELECT `password` FROM Users WHERE `password` = ?;', [password], function (err, rows) {
+	// 	if(err){
+	// 		console.error(err);
+	// 		res.status(404).json({success: false});
+	// 	}else{
+	// 		res.json(rows);
+	// 	}
+	// });
+
+});
+
+
+
+
+
+
+var host = process.env.PORT || 8080;
 
 app.listen(host, console.log("Host is working", host));
